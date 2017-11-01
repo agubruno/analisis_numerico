a6445080ba86e517028232e8bef321945da80fd1
agubruno <agubruno96@gmail.com>
2017-11-01 01:20:16 -0300

    Comienzo de regresion lineal polinomica

--- Analisis Numerico/Entidades/Regresion.cs
+++ Analisis Numerico/Entidades/Regresion.cs
@@ -11,9 +11,11 @@ namespace Entidades
        public double Resultadoa1 { get; set; }
         public double Resultadoa0 { get; set; }
     }
+
+
     public class Regresion
     {
-        public double CoefienteCorrelacion (double[,] coordenadas, int cantidadPuntos, double a1, double a0 )
+        public double CoefienteCorrelacion(double[,] coordenadas, int cantidadPuntos, double a1, double a0)
         {
             double st = 0;
             double sr = 0;
@@ -22,7 +24,7 @@ namespace Entidades
             for (int i = 0; i < cantidadPuntos; i++)
             {
                 promedioY = promedioY + coordenadas[i, 1];
-                st = st + Math.Pow(( a1 * coordenadas[i, 0] + a0 - coordenadas[i, 1]), 2);
+                st = st + Math.Pow((a1 * coordenadas[i, 0] + a0 - coordenadas[i, 1]), 2);
             }
             promedioY = promedioY / cantidadPuntos;
 
@@ -33,7 +35,6 @@ namespace Entidades
 
             r = Math.Sqrt(Math.Abs((st - sr) / st)) * 100;
             return r;
-
 
         }
 
@@ -58,8 +59,54 @@ namespace Entidades
             nuevoResultado.Resultadoa0 = a0;
             nuevoResultado.Resultadoa1 = a1;
 
-            return nuevoResultado ;
+            return nuevoResultado;
         }
 
+        public ResultadoRegresionLineal CalcularRegresionPolimonial(double[,] coordenadas, int cantidadPuntos, int gradoCurva)
+        {
+            ResultadoRegresionLineal nuevoResltado = new ResultadoRegresionLineal();
+            double elevado = 0;
+
+            double[,] Matriz = new double[gradoCurva + 2, gradoCurva + 2];
+
+            Matriz[0, 0] = cantidadPuntos;
+
+            for (int i = 0; i < gradoCurva + 3; i++)
+            {
+                elevado = i;
+                for (int j = 0; i < gradoCurva + 3; i++)
+                {
+                    for (int sumatoria = 0; i < cantidadPuntos + 1; i++)
+                    {
+                        //-----------------------CALCULA COEFICIENTES
+                        if (elevado != 0)
+                        {
+                            Matriz[i, j] = Matriz[i, j] + Math.Pow(coordenadas[1, sumatoria], elevado);
+                        }
+                        //------------------------CALCULA TERMINO INDEPENDIENTE------------------------------------------                
+                        if (j == gradoCurva + 2)
+                        {
+                            if (i == 0)
+                            {
+                                Matriz[i, j] = Matriz[i, j] + coordenadas[0, sumatoria];
+                            }
+                            else
+                            {
+                                Matriz[i, j] = Matriz[i, j] + (coordenadas[0, sumatoria] * Math.Pow(coordenadas[1, sumatoria], i));
+                            }
+                        }
+
+                    }
+                    elevado = elevado + 1;
+                }
+            }
+
+            SistemaDeEcuacion nuevoSistema = new SistemaDeEcuacion();
+            var nuevoRsultadoSistemas = nuevoSistema.CalcularSistemaGaussJordam(Matriz, gradoCurva + 2);
+
+            //Como calculamos coeficiete de grado mayor a 1?
+            return nuevoResltado;
+
+        }
     }
 }
